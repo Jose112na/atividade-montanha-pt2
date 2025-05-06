017c26de7c82b03d9f9aac1274802843f0001db7
Jose112na <124697509+Jose112na@users.noreply.github.com>
2025-05-05 23:59:04 -0300

    att6.java

--- att montanha/att6.java
+++ att montanha/att6.java
@@ -6,8 +6,17 @@ public class att6 {
         Scanner s = new Scanner(System.in);
         int[] v = {1,2,3,4,5,6,7,8,9,10};
         int r = s.nextInt();
-        v = Arrays.stream(v).filter(x -> x != r).toArray();
-        System.out.println(Arrays.toString(v));
+        int[] novoVetor = new int[v.length - 1];
+        int j = 0;
+        for (int num : v) {
+            if (num != r || j == novoVetor.length) {
+                novoVetor[j] = num;
+                j++;
+            }
+        }
+        for (int num : novoVetor) {
+            System.out.print(num + " ");
+        }
         s.close();
     }
 }
