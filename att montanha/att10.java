06dd658b22d27f1898a523f6d6cb73e2de477c63
Jose112na <124697509+Jose112na@users.noreply.github.com>
2025-05-06 00:03:06 -0300

    Update att10.java

--- att montanha/att10.java
+++ att montanha/att10.java
@@ -4,9 +4,22 @@ import java.util.Scanner;
 public class att10 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int[] v = IntStream.range(0, 10).map(i -> s.nextInt()).toArray();
-        Arrays.sort(v);
-        System.out.println(Arrays.toString(v));
+        int[] v = new int[10];
+        for (int i = 0; i < 10; i++) {
+            v[i] = s.nextInt();
+        }
+        for (int i = 0; i < v.length - 1; i++) {
+            for (int j = 0; j < v.length - i - 1; j++) {
+                if (v[j] > v[j + 1]) {
+                    int temp = v[j];
+                    v[j] = v[j + 1];
+                    v[j + 1] = temp;
+                }
+            }
+        }
+        for (int num : v) {
+            System.out.print(num + " ");
+        }
         s.close();
     }
 }
