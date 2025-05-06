3c5ca8e20a0d0fbcb242da05ab2c012e3b59e578
Jose112na <124697509+Jose112na@users.noreply.github.com>
2025-05-06 00:01:05 -0300

    Update att4.java

--- att montanha/att4.java
+++ att montanha/att4.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class att4 {
    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int[] v = new int[20];
-        int contador = 0;
-        for (int i = 0; i < 20; i++) {
+        int[] v = new int[8];
+        for (int i = 0; i < 8; i++) {
             v[i] = s.nextInt();
-            if (v[i] % 2 == 0) {
-                contador++;
-            }
         }
-        System.out.println(contador);
+        for (int i = 7; i >= 0; i--) {
+            System.out.print(v[i] + " ");
+        }
         s.close();
     }
 }
