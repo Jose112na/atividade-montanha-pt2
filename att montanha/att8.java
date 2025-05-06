b2f20c23c562f5ed7ad4952da9e7b63cda58dda4
Jose112na <124697509+Jose112na@users.noreply.github.com>
2025-05-05 23:58:05 -0300

    att8.java

--- att montanha/att8.java
+++ att montanha/att8.java
@@ -1,10 +1,14 @@
 import java.util.Arrays;
 
 public class att8 {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         int[] a = {1,2,3,4,5}, b = {6,7,8,9,10}, c = new int[10];
-        System.arraycopy(a, 0, c, 0, 5);
-        System.arraycopy(b, 0, c, 5, 5);
-        System.out.println(Arrays.toString(c));
+        for (int i = 0; i < 5; i++) {
+            c[i] = a[i];
+            c[i + 5] = b[i];
+        }
+        for (int num : c) {
+            System.out.print(num + " ");
+        }
     }
 }
