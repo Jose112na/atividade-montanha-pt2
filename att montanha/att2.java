26cace1e490d1e8339d510f977ec054bca9ff7ad
Jose112na <124697509+Jose112na@users.noreply.github.com>
2025-05-06 00:01:47 -0300

    Update att2.java

--- att montanha/att2.java
+++ att montanha/att2.java
@@ -5,9 +5,15 @@ public class att2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int[] v = new int[15];
-        for (int i = 0; i < 15; i++) v[i] = s.nextInt();
-        int max = Arrays.stream(v).max().getAsInt();
-        System.out.println(max + " " + Arrays.asList(v).indexOf(max));
+        int maior = Integer.MIN_VALUE, posicao = 0;
+        for (int i = 0; i < 15; i++) {
+            v[i] = s.nextInt();
+            if (v[i] > maior) {
+                maior = v[i];
+                posicao = i;
+            }
+        }
+        System.out.println(maior + " " + posicao);
         s.close();
     }
 }
