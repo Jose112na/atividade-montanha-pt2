0e8d2d8c88ac4c54ed6e751fe039010171c75e5a
Jose112na <124697509+Jose112na@users.noreply.github.com>
2025-05-05 23:57:35 -0300

    Update att9.java

--- att montanha/att9.java
+++ att montanha/att9.java
@@ -3,6 +3,12 @@ import java.util.Arrays;
 public class att9 {
     public static void main(String[] args) {
         int[] v1 = {1,2,3,4,5,6}, v2 = {4,5,6,7,8,9};
-        System.out.println(Arrays.toString(Arrays.stream(v1).filter(x -> Arrays.stream(v2).anyMatch(y -> y == x)).toArray()));
+        for (int num : v1) {
+            for (int num2 : v2) {
+                if (num == num2) {
+                    System.out.print(num + " ");
+                }
+            }
+        }
     }
 }
