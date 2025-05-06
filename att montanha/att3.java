543f2b4114fcd57bd9e509a85fe7a870a69c155f
Jose112na <124697509+Jose112na@users.noreply.github.com>
2025-05-06 00:01:22 -0300

    att3.java

--- att montanha/att3.java
+++ att montanha/att3.java
@@ -5,8 +5,14 @@ public class att3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int[] v = new int[20];
-        for (int i = 0; i < 20; i++) v[i] = s.nextInt();
-        System.out.println(Arrays.stream(v).filter(x -> x % 2 == 0).count());
+        int contador = 0;
+        for (int i = 0; i < 20; i++) {
+            v[i] = s.nextInt();
+            if (v[i] % 2 == 0) {
+                contador++;
+            }
+        }
+        System.out.println(contador);
         s.close();
     }
 }
