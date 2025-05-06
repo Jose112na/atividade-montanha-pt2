608be75a30b58e14ae5b0d5c68c74e3e2a9d982c
Jose112na <124697509+Jose112na@users.noreply.github.com>
2025-05-06 00:02:06 -0300

    Update soma.java

--- att montanha/soma.java
+++ att montanha/soma.java
@@ -3,6 +3,10 @@ import java.util.Arrays;
 public class soma {
     public static void main(String[] args) {
         int[] v = {1,2,3,4,5,6,7,8,9,10};
-        System.out.println(Arrays.stream(v).sum());
+        int soma = 0;
+        for (int num : v) {
+            soma += num;
+        }
+        System.out.println(soma);
     }
 }
