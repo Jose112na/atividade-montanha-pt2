0a2b2e9b4a49114c6dc310f131959dbd21dfe80f
Jose112na <124697509+Jose112na@users.noreply.github.com>
2025-05-05 23:58:31 -0300

    att7.java

--- att montanha/att7.java
+++ att montanha/att7.java
@@ -5,9 +5,16 @@ public class att7 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int[] v = new int[15];
-        for (int i = 0; i < 15; i++) v[i] = s.nextInt();
-        int n = s.nextInt();
-        System.out.println(Arrays.stream(v).filter(x -> x == n).count());
+        for (int i = 0; i < 15; i++) {
+            v[i] = s.nextInt();
+        }
+        int n = s.nextInt(), contador = 0;
+        for (int num : v) {
+            if (num == n) {
+                contador++;
+            }
+        }
+        System.out.println(contador);
         s.close();
     }
 }
