ad105070813bd9302f8d54b8b8f6461f917b2b7e
Jose112na <124697509+Jose112na@users.noreply.github.com>
2025-05-05 23:59:30 -0300

    att5.java

--- att montanha/att5.java
+++ att montanha/att5.java
@@ -5,10 +5,19 @@ public class att5 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int[] v = new int[12];
-        for (int i = 0; i < 12; i++) v[i] = s.nextInt();
-        double media = Arrays.stream(v).average().getAsDouble();
+        int soma = 0, contador = 0;
+        for (int i = 0; i < 12; i++) {
+            v[i] = s.nextInt();
+            soma += v[i];
+        }
+        double media = soma / 12.0;
         System.out.println(media);
-        System.out.println(Arrays.stream(v).filter(x -> x > media).count());
+        for (int num : v) {
+            if (num > media) {
+                contador++;
+            }
+        }
+        System.out.println(contador);
         s.close();
     }
 }
